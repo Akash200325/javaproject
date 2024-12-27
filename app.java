0bb2a731ef8bf5e28d723be93df39b61775ce1e9
Akash200325 <142168006+Akash200325@users.noreply.github.com>
2024-12-27 22:48:27 +0530

    Update app.java

--- app.java
+++ app.java
@@ -1,7 +1,7 @@
 package com.example;
 
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public String greet(String name) {
+        return "Hello, " + name;
     }
 }
